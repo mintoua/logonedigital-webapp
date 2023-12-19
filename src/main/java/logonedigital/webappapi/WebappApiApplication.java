0f5c196d054be672c61d759882318e246c273b63
mintoua <toupkandi.mintoua@esprit.tn>
2023-12-19 21:16:37 +0100

    created project

---
+++ src/main/java/logonedigital/webappapi/WebappApiApplication.java
@@ -0,0 +1,13 @@
+package logonedigital.webappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/logonedigital/webappapi/WebappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package logonedigital.webappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
