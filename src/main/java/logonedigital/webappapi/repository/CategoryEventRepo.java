2b900e3f85a918782126d6010594ddf21a9ea6b5
NGUE EMMANUEL <lesliebrielle.yepdomonthe@esprit.tn>
2023-12-22 13:18:52 +0100

    configuration de swagger et mysql

---
+++ src/main/java/logonedigital/webappapi/repository/CategoryEventRepo.java
@@ -0,0 +1,7 @@
+package logonedigital.webappapi.repository;
+
+import logonedigital.webappapi.entity.CategoryEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryEventRepo extends JpaRepository<CategoryEvent, Integer> {
+}
