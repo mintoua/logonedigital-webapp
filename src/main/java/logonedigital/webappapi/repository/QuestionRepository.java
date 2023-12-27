da2eaf8c12110ac63f06d5b12d910ff186007d94
mintoua <toupkandi.mintoua@esprit.tn>
2023-12-27 19:31:54 +0100

    fixed bean creation error

--- src/main/java/logonedigital/webappapi/repository/QuestionRepository.java
+++ src/main/java/logonedigital/webappapi/repository/QuestionRepository.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question,Integer> {
-    List<Question> findBySujetId(Integer sujetId);
     @Query("SELECT DISTINCT q.subject FROM Question q")
     List<String> findDistinctSubject();
     Page<Question> findBySubject(String subject, Pageable pageable);
--- target/generated-sources/annotations/logonedigital/webappapi/mapper/EventFeatureMapperImpl.java
+++ target/generated-sources/annotations/logonedigital/webappapi/mapper/EventFeatureMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-12-27T18:15:41+0100",
-    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 19.0.1 (Oracle Corporation)"
+    date = "2023-12-27T18:56:37+0100",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 20.0.1 (Oracle Corporation)"
 )
 @Component
 public class EventFeatureMapperImpl implements EventFeatureMapper {
